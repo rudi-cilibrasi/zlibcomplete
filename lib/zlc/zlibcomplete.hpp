9938cb258c0ac13ec644051a13180760c46dcb99
Rudi Cilibrasi <rudi@morefuntechnologies.com>
2015-07-07 13:25:02 -0700

    Fixing circle.yml

--- lib/zlc/zlibcomplete.hpp
+++ lib/zlc/zlibcomplete.hpp
@@ -9,6 +9,17 @@
 #error "Please upgrade your zlib library for security."
 #endif
 
+#ifdef __GNUC__
+
+         #define CURRENT_GCC_VERSION (__GNUC__ * 10000 \
+                                + __GNUC_MINOR__ * 100 \
+                                + __GNUC_PATCHLEVEL__)
+#if CURRENT_GCC_VERSION < 40800
+#error "Please use g++ version 4.8 or later."
+#endif
+
+#endif
+
 #include <zlc/gzipcomplete.hpp>
 #include <zlc/zlibmisc.hpp>
 #include <zlc/zlibraw.hpp>
